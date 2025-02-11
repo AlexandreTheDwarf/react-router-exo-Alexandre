6f541e625414cd1885200c63321152ababfd40ab
AlexandreTheDwarf <alexandre-vandewiele@outlook.com>
2025-02-11 15:09:50 +0100

    save after technology page

--- src/pages/Crew/Crew.jsx
+++ src/pages/Crew/Crew.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./Crew.scss";
+import { GoDotFill } from "react-icons/go";
+import { GoDot } from "react-icons/go";
 import NavBar from '../../components/NavBar/NavBar';
 
 function Crew() {
@@ -11,7 +13,29 @@ function Crew() {
           Votre navigateur ne supporte pas la vidéo.
         </video>
       </div>
+      <div class="Overlay"></div>
       <NavBar />
+      <div className='CrewDivBottom'>
+        <div className='DivText'>
+            <div className='Title'> 
+                <h3><span>02</span> MEET YOUR CREW</h3>
+            </div>
+            <div className='Description'>
+                <h2>COMMANDER</h2>
+                <h1>DOUGLAS HURLEY</h1>
+                <p>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+            </div>
+            <div className='menu'>
+                <GoDot />
+                <GoDotFill />
+                <GoDotFill />
+                <GoDotFill />
+            </div>
+        </div>
+        <div className='DivImage'>
+                <img src="/img/image-douglas-hurley.webp" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Technology/Technology.jsx
+++ src/pages/Technology/Technology.jsx
@@ -6,6 +6,28 @@ function Technology() {
   return (
     <div className='Technology'>
         <NavBar/>
+        <div className='title'>
+          <h2><span>03</span> SPACE LAUNCH 101</h2>
+        </div>
+        <div className='TechnologyBot'>
+          <div className='left'>
+              <div className='container'>
+                <div className='contLeft'>
+                  <span className='selected'>1</span>
+                  <span>2</span>
+                  <span>3</span>
+                </div>
+                <div className='contRight'>
+                    <h4>THE TERMINOLOGY ...</h4>
+                    <h1>LAUNCH VEHICULE</h1>
+                    <p>Le vaisseau de transport Helldivers, un hybride entre avion et hélicoptère, est conçu pour des missions de déploiement rapide. Équipé de technologies avancées, il assure une insertion précise et sécurisée des troupes. Sa conception robuste et ses capacités de vol stationnaire en font un atout inestimable sur le champ de bataille. Doté de systèmes de défense intégrés.</p>
+                </div>
+              </div>
+          </div>
+          <div className='right'>
+              <img src="/img/ship.jpg" alt="" />
+          </div>
+        </div>
       </div>
   )
 }
